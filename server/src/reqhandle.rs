915a1bc37827cf0be197c1d6bd799949703c5c74
Martin Jediny <jedinym@gmail.com>
2022-07-24 11:29:10 +0200

    Restructure code better

---
+++ server/src/reqhandle.rs
@@ -0,0 +1,74 @@
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+use crate::response::{ResponseBuilder, Response};
+
+extern crate httparse;
+use httparse::{Request, Header, EMPTY_HEADER};
+
+use log::{info, error};
+
+enum RequestError {
+    UnknownRoute,
+    WrongMethod
+}
+
+
+fn handle_request(_req: &Request) -> Response {
+    let test = ResponseBuilder::success()
+        .body("BODYYY!".to_owned()).build();
+
+    test
+}
+
+
+fn parse_request<'a>(
+    stream: &'a mut TcpStream,
+    headers: &'a mut [Header<'a>],
+    buf: &'a mut Vec<u8>)
+    -> Result<Request<'a, 'a>, httparse::Error> {
+    let mut request = Request::new(headers);
+
+    stream.read(buf).unwrap();  // TODO: is this secret? is this safe?
+    let _res = request.parse(buf)?;
+
+    Ok(request)
+}
+
+
+fn send_response(stream: &mut TcpStream, response_bytes: Vec<u8>) {
+    stream.write(&response_bytes).unwrap();
+    stream.flush().unwrap();
+}
+
+
+fn log_request(req: &Request, response: &Response) {
+    let method = &req.method.unwrap();
+    let path = &req.path.unwrap();
+    info!{"{} - {} - {}", method, path, response.status_code};
+}
+
+
+pub fn handle_client(stream: &mut TcpStream) {
+    let mut buf = vec![0; 1024];
+    let mut headers = [EMPTY_HEADER; 64];
+    let request_result = parse_request(stream, &mut headers, &mut buf);
+
+    let response = match request_result {
+        Err(errmsg) => {
+            error!("Could not parse request: {}", errmsg);
+            let response_bytes = ResponseBuilder::bad_request()
+                .body(errmsg.to_string())
+                .build().bytes();
+            response_bytes
+        }
+        Ok(request) => {
+            let response = handle_request(&request);
+            log_request(&request, &response);
+            let response_bytes = response.bytes();
+            response_bytes
+        }
+    };
+
+    send_response(stream, response)
+}
--- server/src/response.rs
+++ server/src/response.rs
@@ -1,32 +1,76 @@
+
 pub struct Header {
     pub name: String,
     pub value: String,
 }
 
+impl Clone for Header {
+    fn clone(&self) -> Header {
+        Header {name: self.name.clone(), value: self.value.clone()}
+    }
+}
+
+pub struct Response {
+    pub status_code: u16,
+    pub headers: Vec<Header>,
+    pub body: String
+}
+
+
+impl Clone for Response {
+    fn clone(&self) -> Response {
+        Response {
+            status_code: self.status_code,
+            headers: self.headers.clone(),
+            body: self.body.clone()
+        }
+    }
+}
+
 pub struct ResponseBuilder {
-    _status_code: u16,
-    headers: Vec<Header>,
-    body: String,
+    response: Response
+}
+
+
+impl Response {
+    pub fn bytes(&self) -> Vec<u8> {
+        let mut response = String::new();
+        response.push_str("HTTP/1.1 ");
+        response.push_str(&self.status_code.to_string());
+        response.push_str(" Testing-phrase ");
+        response.push_str("\r\n");
+
+        for header in &self.headers {
+            let a: String = format!("{}:{} ", header.name, header.value);
+            response.push_str(&a);
+        }
+
+        response.push_str("\r\n");
+        response.push_str(&self.body);
+
+        response.as_bytes().to_vec()
+    }
 }
 
 impl ResponseBuilder {
     pub fn new() -> ResponseBuilder {
-        ResponseBuilder { _status_code: 200, headers: Vec::new(), body: String::new() }
+        let response = Response { status_code: 200, headers: Vec::new(), body: String::new() };
+        ResponseBuilder { response }
     }
 
     pub fn status_code(mut self, code: u16) -> ResponseBuilder {
-        self._status_code = code;
+        self.response.status_code = code;
         self
     }
 
     pub fn add_header(mut self, header: Header) -> ResponseBuilder {
-        self.headers.push(header);
+        self.response.headers.push(header);
         self
     }
 
     pub fn body(mut self, bytes: String) -> ResponseBuilder {
-        self.body = bytes;
-        self.headers.push(Header { name: "Content-Length".to_owned(), value: self.body.len().to_string() });
+        self.response.body = bytes;
+        self.response.headers.push(Header { name: "Content-Length".to_owned(), value: self.response.body.len().to_string() });
         self
     }
 
@@ -40,21 +84,7 @@ impl ResponseBuilder {
             .status_code(400)
     }
 
-    pub fn build(&mut self) -> Vec<u8> {
-        let mut response = String::new();
-        response.push_str("HTTP/1.1 ");
-        response.push_str(&self._status_code.to_string());
-        response.push_str(" Testing-phrase ");
-        response.push_str("\r\n");
-
-        for header in &self.headers {
-            let a: String = format!("{}:{} ", header.name, header.value);
-            response.push_str(&a);
-        }
-
-        response.push_str("\r\n");
-        response.push_str(&self.body);
-
-        response.as_bytes().to_vec()
+    pub fn build(&mut self) -> Response {
+        self.response.clone()
     }
 }
