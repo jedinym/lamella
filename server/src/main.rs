d77de90ed9acb61c5750572af2dfaa3c9d2ba5ae
Martin Jediny <jedinym@gmail.com>
2022-08-08 21:07:17 +0200

    Streamlined threadpool with graceful shutdown

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,16 +3,24 @@ mod response;
 mod reqhandle;
 
 extern crate simple_logger;
+extern crate ctrlc;
 
 use std::net::TcpListener;
+use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
 
-use threadpool::TcpTask;
-use threadpool::Threadpool;
+use log::info;
+use threadpool::{Threadpool, Message, Job};
 
 use reqhandle::handle_client;
 
 use simple_logger::SimpleLogger;
 
+fn set_ctrlc_handler(should_exit: Arc<AtomicBool>) {
+    ctrlc::set_handler(move || {
+        info!("Attempting graceful shutdown");
+        should_exit.store(true, Ordering::Relaxed);
+    }).expect("Couldn't set interrupt handler");
+}
 
 fn main() {
     SimpleLogger::new().env().init().unwrap();
@@ -22,14 +30,26 @@ fn main() {
     let addr = "0.0.0.0";
     let port = "8000";
 
-    let listener = TcpListener::bind(format!("{}:{}", addr, port)).unwrap();
+    let should_exit = Arc::new(AtomicBool::new(false));
+    set_ctrlc_handler(should_exit.clone());
+
+    let listener = TcpListener::bind(format!("{}:{}", addr, port))
+        .expect("Could not create TCP listener");
+
     println!("Server listening at {}:{}", addr, port);
     println!("Workers running: {}", n_workers);
 
     loop {
-        let (stream, _addr) = listener.accept().unwrap();
-        let task = TcpTask::new(handle_client, stream);
+        if should_exit.load(Ordering::Relaxed) {
+            pool.exit().expect("Failed to shutdown gracefully");
+            info!("Graceful shutdown successful");
+            return;
+        }
 
-        pool.add_task(task);
+        let (stream, _addr) = listener.accept().unwrap();
+        let job = Job::new(stream, handle_client);
+        let msg = Message::NewJob(job);
+
+        pool.add_task(msg);
     }
 }
