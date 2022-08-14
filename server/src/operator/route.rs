aab93ea369ae839bf99b511fcdfdb6a914ffcad9
Martin Jediny <jedinym@gmail.com>
2022-08-14 17:13:14 +0200

    Implement testing routing

---
+++ server/src/operator/route.rs
@@ -0,0 +1,12 @@
+use httparse::Request;
+
+#[derive (Debug)]
+pub enum RouteError {
+    UnknownRoute,
+    RouteNotMatched,
+    MissingParameter(&'static str),
+}
+
+pub trait Route: Send {
+    fn resolve(&mut self, req: &Request) -> Result<String, RouteError>;
+}
